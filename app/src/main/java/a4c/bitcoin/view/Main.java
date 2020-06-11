e32a9a9fe5400d416c741ae3908e17c89c188a8b
rama333 <ramil-faxrut@yandex.ru>
2020-06-11 10:00:10 +0400

    add project

--- app/src/main/java/a4c/bitcoin/view/Main.java
+++ app/src/main/java/a4c/bitcoin/view/Main.java
@@ -6,6 +6,8 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 import a4c.bitcoin.R;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by Ramil on 25.08.2017.
@@ -16,6 +18,7 @@ public class Main extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.main);
 
         Handler handler = new Handler();
